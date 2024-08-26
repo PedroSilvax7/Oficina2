9ea764502cfbda49dced636db225da790366cf3e
PedroSilvax7 <145567161+PedroSilvax7@users.noreply.github.com>
2024-08-26 08:35:07 -0300

    fasecompletajogo

--- AtividadeMotores2/Assets/Scripts/GameController.cs
+++ AtividadeMotores2/Assets/Scripts/GameController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public int totalScore;
     public Text scoreText;
+
+    public GameObject gameOver;
     
     public static GameController instance;
     // Start is called before the first frame update
@@ -18,6 +20,16 @@ public class GameController : MonoBehaviour
     public void UpdateScoreText()
     {
         scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
     }
 
     // Update is called once per frame
